2036c201a20cdd28fa569bbe44fab2db7eaea4d7
efgarro <efgarro@yahoo.com>
2025-04-20 06:46:39 -0600

    0420b

--- src/RouteHandlers/cmAppHandlers.js
+++ src/RouteHandlers/cmAppHandlers.js
@@ -3,6 +3,7 @@ import * as db from "../Config/postgresPool.js";
 import busboy from "busboy";
 import { pipeline } from "node:stream";
 import ExifReader from "exifreader";
+import { v7 as uuidv7 } from "uuid";
 
 import multer from "multer";
 import { storageR2 } from "../Config/multer.js";
@@ -39,21 +40,45 @@ export const parseImageFile = (req, res, next) => {
 export const uploadToR2 = multer({ storage: storageR2 });
 
 export const registerNewPlace = async (req, res, next) => {
-  const { place_id, place_type, hub, name, description, latitude, longitude } =
-    req.body;
+  const { place_type, hub, name, description, latitude, longitude } = req.body;
+  const place_id = uuidv7();
+  const image_set_id = uuidv7();
 
   switch (place_type) {
     case "lodge":
-      const { email } = req.body;
+      const lodge_id = uuidv7();
       try {
-        await db.query(
-          `INSERT INTO scr_places (place_id, schema_type, hub, name, description, latitude,  longitude) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-          [place_id, place_type, hub, name, description, latitude, longitude]
-        );
-        await db.query(
-          `INSERT INTO scr_lodging (lodge_id, place_id, email) VALUES (uuid_generate_v7(), $1, $2)`,
-          [place_id, email]
-        );
+        await db.query(`SELECT register_lodge($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, [
+          place_id,
+          lodge_id,
+          image_set_id,
+          place_type,
+          hub,
+          name,
+          description,
+          latitude,
+          longitude,
+          req.body.email
+        ]);
+      } catch (err) {
+        console.log(err);
+      }
+      next();
+    case "resta":
+      const resta_id = uuidv7();
+      try {
+        await db.query(`SELECT register_resta($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, [
+          place_id,
+          resta_id,
+          image_set_id,
+          place_type,
+          hub,
+          name,
+          description,
+          latitude,
+          longitude,
+          req.body.email
+        ]);
       } catch (err) {
         console.log(err);
       }
