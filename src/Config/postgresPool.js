58c6454377a76737eb030d799c42554c4e8b2713
Eduardo Garro <efgarro@yahoo.com>
2025-11-28 04:57:09 -0600

    cheap-query 14

--- src/Config/postgresPool.js
+++ src/Config/postgresPool.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({
-  path: "/home/ubuntu/actions-runner-2024v10-scr-api/_work/2024v10-SCR-API/2024v10-SCR-API/.env",
+  path: "../../.env",
 });
 
 import pg from "pg";
