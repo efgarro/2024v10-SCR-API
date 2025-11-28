1dde1862bf35e43ad848064ec7ff7b8be55e0660
Eduardo Garro <efgarro@yahoo.com>
2025-11-28 05:38:20 -0600

    cheap-query 21

--- src/Config/postgresPool.js
+++ src/Config/postgresPool.js
@@ -1,11 +1,12 @@
+import { join } from "node:path";
 import dotenv from "dotenv";
 // dotenv.config({
 //   path: "/home/ubuntu/actions-runner-2024v10-scr-api/_work/2024v10-SCR-API/2024v10-SCR-API/.env",
 // });
-console.log(import.meta.dirname);
+console.log(join(import.meta.dirname, "../../.env"));
 
 dotenv.config({
-  path: import.meta.dirname + "../../.env",
+  path: join(import.meta.dirname, "../../.env"),
 });
 
 // import {env} from "../../.env"
