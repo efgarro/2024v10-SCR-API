6346f45a5721dd52b135f277c8894a904adf2426
Eduardo Garro <efgarro@yahoo.com>
2025-11-27 19:43:27 -0600

    cheap-query 4

--- src/RouteHandlers/blnApiHandlers.js
+++ src/RouteHandlers/blnApiHandlers.js
@@ -61,7 +61,8 @@ export const getImageSetStack = async (req, res) => {
 export const getCheapQuery = async (req, res) => {
   let response;
   try {
-    response = await blnQuery(`SELECT * FROM bln_image_clusters`);
+    response = await blnQuery(`SELECT 10`);
+    // response = await blnQuery(`SELECT * FROM bln_image_clusters`);
   } catch (err) {
     console.log(err);
   }
