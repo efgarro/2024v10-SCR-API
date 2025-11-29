6fbb5a87dd418ebc9c0421125c66748c4655d5e4
Eduardo Garro <efgarro@yahoo.com>
2025-11-28 19:59:11 -0600

    cheap-query 27

--- src/Routers/blnApiRouter.js
+++ src/Routers/blnApiRouter.js
@@ -26,10 +26,11 @@ blnApiRouter.get("/cluster/select_cluster", selectClusterToAddImages);
 
 // BijaLapa Natural Website
 blnApiRouter.param("image_cluster_id", (req, res, next, image_cluster_id) => {
+  console.log(image_cluster_id);
   req.body.image_cluster_id = image_cluster_id;
   next();
 });
 
 blnApiRouter.get("/cluster/cluster_stack/:image_cluster_id", getImageSetStack);
 
-blnApiRouter.get("/cheap-query", getCheapQuery)
+blnApiRouter.get("/cheap-query", getCheapQuery);
