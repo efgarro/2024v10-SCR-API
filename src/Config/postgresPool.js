16962b264acdfb4c9f1ab79cbb4a260619f5ae63
Eduardo Garro <efgarro@yahoo.com>
2025-12-06 13:01:12 -0600

    userpwd

--- src/Config/postgresPool.js
+++ src/Config/postgresPool.js
@@ -5,8 +5,8 @@ dotenv.config({
   path: join(import.meta.dirname, "../../.env"),
 });
 
-console.log(process.env.PG_USER)
-console.log(process.env.PG_PWD)
+console.log(process.env.PG_USER);
+console.log(process.env.PG_PWD);
 
 import pg from "pg";
 const { Pool } = pg;
@@ -25,8 +25,15 @@ const blnPool = new Pool({
   host: "54.205.165.107",
   port: 5432,
   database: "2025v12-bln-db",
-  user: process.env.PG_USER,
-  password: process.env.PG_PWD,
+  user: "efgarro",
+  password: "Due427ga",
 });
+// const blnPool = new Pool({
+//   host: "54.205.165.107",
+//   port: 5432,
+//   database: "2025v12-bln-db",
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PWD,
+// });
 
 export const blnQuery = (text, params) => blnPool.query(text, params);
